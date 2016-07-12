b94433cb2b894cd5d818dec46bf7598dda8a98b9
mutaihillary <mutaihillary@yahoo.com>
2016-07-12 23:02:07 +0300

    add

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).foundation()
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(2000)
+    .next()
+    .fadeIn(2000)
+    .end()
+    .appendTo('#slideshow');
+},  6000);
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
